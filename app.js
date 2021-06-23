d11e0420b3b7fe7627bb28141f63b70c926600d3
Ezequiel Visintini <ezeq1802@gmail.com>
2021-06-22 22:25:25 -0300

    video 8

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const { inquirerMenu } = require('./helpers/inquirer');
+
+
 
 
 console.clear();
 
-
+ 
 const main = async() => {    
     console.log('Hola Mundo'); 
 
     let opt = '';
 
     do {
-        opt = await mostrarMenu();
+        opt = await inquirerMenu();
         console.log({opt});
 
-        if ( opt !== '0' ) await pausa();
 
     } while(opt != '0' );
 
